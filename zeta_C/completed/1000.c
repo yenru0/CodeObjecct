dd9dfae8d4a775517adaf3e0a8c5711683c5c301
yenru0 <yenru0604@gmail.com>
2024-04-05 14:31:57 +0900

    complete 1000 with korean C

---
+++ zeta_C/completed/1000.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+#define 정수 int
+#define 반환 return
+#define 출력 printf
+#define 입력 scanf
+#define 더하기 +
+
+정수 main() {
+    정수 에이, 비;
+    입력("%d %d", &에이, &비);
+    출력("%d\n", 에이 더하기 비);
+
+    반환 0;
+}

52ece06ea44688a8bee5837cb48c26a345bd41e0
yenru0 <yenru0604@gmail.com>
2024-07-03 23:57:11 +0900

    complete 2776.py

---
+++ zeta_python/completed/2776.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    T = int(input())
+    for _ in range(T):
+        N1 = int(input())
+        I1 = set(map(int, input().split()))
+        N2 = int(input())
+        I2 = list(map(int, input().split()))
+        for i in I2:
+            if i in I1:
+                print(1)
+            else:
+                print(0)
